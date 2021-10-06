85ef7c03a9c3ecf846fce993a8069df918a8eed4
Nathan Flessner <nathanflessner1@gmail.com>
2021-10-05 20:07:23 -0600

    added sendgrid functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+const { User } = require('./models');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -20,8 +21,8 @@ const sess = {
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -37,9 +38,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 app.get('/', (req, res) => {
-  res.render('main')
+  res.render('main');
 });
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
+// sendgrid information and functions
+// const sgMail = require('@sendgrid/mail')
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+User.findAll().then((users) => {
+  const userData = users.map((user) => user.dataValues.email);
+
+
+const msg = {
+  to: userData, // Change to your recipient
+  from: 'scryptsofbetrayal@gmail.com', // Change to your verified sender
+  subject: 'Are you enjoying our game?',
+  text: 'and easy to do anywhere, even with Node.js',
+}
+const onTickHandler = async () => {
+  try {
+    // Make the api request to sendgrid to send an email.
+    await sgMail.send(msg)
+    console.log('email sent successfully:', msg)
+  } catch (error) {
+    console.error(error)
+
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  }
+}});
